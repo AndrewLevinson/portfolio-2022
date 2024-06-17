d3141a5ca37387143c3545147f958a0cecc33c56
Andrew Levinson <andrewlevins@gmail.com>
2024-06-16 22:51:10 -0400

    add basic markup

--- nowPlaying.mjs
+++ nowPlaying.mjs
@@ -3,4 +3,27 @@ fetch('/api/spotify-track')
   .then(res => res.json())
   .then(data => {
     console.log('hi', data);
+    const songInfo = document.querySelector('.song-into');
+
+    songInfo.innerHTML = `
+      <h5>
+            Last song:
+          </h5>
+         <div class="flex">
+            <img src=${data.albumImageUrl} alt={album image for ${data.album} by ${data.artist}} width={70}
+              height={70} />
+            <div class="info">
+              <span>${data.title}</span>
+              <span>${data.artist}</span>
+              <div class="genreList">
+                ${data.genreList
+                  .slice(0, 3)
+                  .map(genre => genre)
+                  .join(', ')
+                  .replace(/, ([^,]*)$/, ', and $1')}
+              </div>
+            </div>
+          </div>
+      
+      `;
   });
